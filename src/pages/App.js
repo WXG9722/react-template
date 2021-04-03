ec5ada872fcea7e4d2d0cac855bda047e2d96154
xinguang.wang <1321844625@qq.com>
2021-04-03 12:56:58 +0800

    init

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,17 +1,21 @@
+import store from '../store/store';
+import zhCN from 'antd/lib/locale/zh_CN';
 import { Suspense, lazy } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../store/store';
+import { ConfigProvider } from 'antd';
 
 function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={null}>
         <Provider store={store}>
-          <Switch>
-            <Route path="/hooks_example" component={lazy(() => import('./HooksExample'))} />
-            <Route path="/class_example" component={lazy(() => import('./ClassExample'))} />
-          </Switch>
+          <ConfigProvider locale={zhCN}>
+            <Switch>
+              <Route path="/hooks_example" component={lazy(() => import('./HooksExample'))} />
+              <Route path="/class_example" component={lazy(() => import('./ClassExample'))} />
+            </Switch>
+          </ConfigProvider>
         </Provider>
       </Suspense>
     </BrowserRouter>
